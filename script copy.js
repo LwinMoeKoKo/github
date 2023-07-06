b2e913503b9dcfb28030ad259f93d8d67bddc076
Lwin Moe Ko Ko <lmkklmkk05@gmail.com>
2023-07-06 18:30:39 +0630

    Create script copy.js
    Update

---
+++ script copy.js
@@ -0,0 +1,11 @@
+console.log("No Pain,No Gain");
+const value = 1;
+const divTag = document.createElement("div");
+
+dispatchEvent.apply("");
+
+setInterval(() => {
+    console.log("Rain");
+}, 500);
+
+console.log("Github");
